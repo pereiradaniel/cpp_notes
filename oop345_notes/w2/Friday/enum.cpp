72dc69c7ad940a991a5cfae973542e3d233f917d
Daniel Pereira <danielpaulpereira@protonmail.com>
2022-01-21 09:48:20 -0500

    Add more

--- oop345_notes/w2/Friday/enum.cpp
+++ oop345_notes/w2/Friday/enum.cpp
@@ -28,6 +28,21 @@ void paintHouse(Colours colour)
     cout << endl;
 }
 
+// First 4 bytes: INT, Next 8 bytes: DOUBLE, Next 32 bytes char[]
+struct StrucData // 44 bytes
+{
+    int m_int;          //4
+    double m_double;    //8
+    char m_strp[32];    //32
+};
+
+union UnionData // Will have the size of the biggest field (32 bytes)
+{
+    int m_int;          //4
+    double m_double;    //8
+    char m_strp[32];    //32
+};
+
 int main()
 {
     paintHouse(Colours::Blue);
