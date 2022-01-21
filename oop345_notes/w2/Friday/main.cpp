0f9c514bb7aa99df8cf213f108c49aefea1a6253
Daniel Pereira <danielpaulpereira@protonmail.com>
2022-01-21 08:19:56 -0500

    add more

--- oop345_notes/w2/Friday/main.cpp
+++ oop345_notes/w2/Friday/main.cpp
@@ -2,9 +2,17 @@
 #include <utility>
 using namespace std;
 
-class Number
+class Numbers
 {
-    // Make a resource that is a collection of numbers: Pointer to an array. 
+    // Make a resource that is a collection of numbers: Pointer to an array.
+    short* m_resource{}; // initialize the pointer with nullptr
+
+    // A method to determine the size of the dynamically created array is required!
+    size_t m_size{}; // Do not leave this uninitialized! nullptr
+public:
+    // custom implementation of copy operation
+    // what is prototype of copy constructor?
+    Numbers(const Numbers&); // Do not forget const!
 
 };
 
