d7c6470906104497aeffbdcc7dd7036fa4dd896b
Daniel Pereira <danielpaulpereira@protonmail.com>
2022-01-21 08:15:29 -0500

    Adding w2

---
+++ oop345_notes/w2/Friday/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <utility>
+using namespace std;
+
+class Number
+{
+    // Make a resource that is a collection of numbers: Pointer to an array. 
+
+};
+
+int main()
+{
+    // TODO: Rule of 5
+    // TODO: Enumerations
+
+}
