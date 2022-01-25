c8a4c9f78358a351e264111467693154bbedc084
pereiradaniel <danielpaulpereira@protonmail.com>
2022-01-25 16:19:51 -0500

    Update

--- oop345_notes/w3/main.cpp
+++ oop345_notes/w3/main.cpp
@@ -1,7 +1,9 @@
 // OOP345 w3 notes
 // review parametric polymorphism
+
 #include <iostream>
 using namespace std;
+
 class Animal
 {
     public:
@@ -9,11 +11,14 @@ class Animal
     virtual void eat() = 0; // pure virtual function
 };
 
+#pragma region Dog
 class Dog : public Animal
 {
+    std::string m_name = "Hunter";
 public:
-    virtual void move();
-    virtual void eat();
+    virtual void move() override;
+    virtual void eat() override;
+    void bark();
 };
 
 void Dog::move()
@@ -26,9 +31,64 @@ void Dog::eat()
     cout << "The dog is eating...\n";
 }
 
+void Dog::bark()
+{
+    cout << m_name << " barks.\n";
+}
+
+#pragma region endregion Dog
+
+#pragma region Cat
+class Cat:public Animal
+{
+public:
+    void move() override;
+    void eat() override;
+};
+
+void Cat::move()
+{
+    cout << "Cat jumps into a tre.\n";
+}
+
+void Cat::eat()
+{
+    cout << "Cat caught a mouse and eats it.\n";
+}
+#pragma endregion Cat
+
+// class MechanicalDog:public Dog
+// {
+// public:
+//     void eat();
+// };
+
 int main()
 {
-    Dog theDog;
+    Animal* theAnimal{}; // Cannot create an instance of type animal, make a pointer
+    char choice = '\0';
+    cout << "What animal? (d/c) ";
+    cin >> choice;
+    if (choice == 'd')
+        theAnimal = new Dog; // instantiate Dog class
+    else
+        theAnimal = new Cat;
+    
+    theAnimal->move(); // late binding or dynamic dispatch
+    theAnimal->eat();
+
+    // Dynamic cast to force Cat to bark
+    Dog* ptrDog = dynamic_cast<Dog*>(theAnimal);
+    // How do I check if this is a Dog?
+    // Dynamic case returns NULL if the conversion is not possible
+    if (ptrDog != nullptr)
+        ptrDog->bark();
+    else
+        cout << "Not a dog!\n";
+
+    // theAnimal->bark();
+    
+    // theAnimal2 = new MechanicalDog();
 
     return 0;
 }
