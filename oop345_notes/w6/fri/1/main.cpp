313538b145fd565d5408543c7e269b45bd0807b6
Daniel Pereira <danielpaulpereira@protonmail.com>
2022-02-18 09:16:13 -0500

    update

--- oop345_notes/w6/fri/1/main.cpp
+++ oop345_notes/w6/fri/1/main.cpp
@@ -15,15 +15,26 @@ void func(const char* data)
     cout << "Hello from function. [" << data << "]\n";
 };
 
+class Foo
+{
+    public:
+        void operator()(const char* data) const
+        {
+            cout << "Hello from functors. [" << data << "\n]";
+        }
+};
+
 int main()
 {
 
     // TODO: stack & queue
-    std::function<void (const char*)> f1 = func;  // function object
+    std::function<void (const char*)> f1 = func;
     std::function<void (const char*)> f2 = [](const char* data) {
         cout << "Hello from lambda. [" << data << "]\n";
     };
+    std::function<void (const char*)> f3 = func;
 
     f1("C++");
     f2("C++ with lambda");
+    f3("C++ with function object.");
 }
